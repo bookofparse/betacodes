d7c327a2879170efba8f328c156294caca90b528
jack Lee <evileye2000@naver.com>
2017-01-04 00:53:54 +0900

    remove unused files

--- Chapter_8/raspberry/8_2_6_PARSE_QUERY_확인.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <parse.h>
-#define PARSE_URL  "http://xxx.herokuapp.com/parse"
-int main(int argc, char *argv[]) {
-  ParseClient client = parseInitializeWithServerURL("myAppId", "myAppId", PARSE_URL);
-  parseSendRequest(client, "POST", "/classes/TestPi", "{\"name\":\"room_5\",\"bright\":100}", NULL);
-  return 0;
-}
